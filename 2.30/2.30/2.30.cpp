f004101c159bb53d7fc7ddb7f1999381d8c94dd1
Squeak <squeakasaur@live.com>
2014-06-07 18:16:35 -0700

    syncing 2.30

---
+++ 2.30/2.30/2.30.cpp
@@ -0,0 +1,16 @@
+// Excercise 2.30 BMI Calculator
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int weight = 0;
+	int height = 0;
+	cout << "Enter your weight (lbs):";
+	cin >> weight;
+
+	cout << "Enter your height (in):";
+	cin >> height;
+}
