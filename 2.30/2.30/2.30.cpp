4eb509f7396ce89ca2ba65e41eca6850cdf65e0b
Squeak <squeakasaur@live.com>
2014-06-07 19:59:25 -0700

    BMIcalc

--- 2.30/2.30/2.30.cpp
+++ 2.30/2.30/2.30.cpp
@@ -13,4 +13,10 @@ int main()
 
 	cout << "Enter your height (in):";
 	cin >> height;
+
+	int body = (weight * 703) / (height * height);
+
+	cout << "Your BMI is:" << body;
+
+	cout << "\n\nImportant information about BMI Values\n Underweight: Less than 18.5\n Normal: between 18.5 and 24.9\n Overweight: between 25 and 29.9\n Obese: 30 or greater\n";
 }
