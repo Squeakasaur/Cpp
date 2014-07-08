79c6cc2d9998de7ac9955a1e2884462a5b2b7fc5
Squeak <squeakasaur@live.com>
2014-07-08 13:24:45 -0700

    class added

--- 3.13/3.13/3.13.cpp
+++ 3.13/3.13/3.13.cpp
@@ -1,9 +1,34 @@
 //Excercise 3.13 Invoice class
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+class invoice
+{
+public:
+
+	void invoicedetails(string Details1, string Details2, string Details3, string Details4)
+	{
+		cout << "Invoice Number\n" << Details1 << endl;
+		
+
+	}
+
+
+};
+
+
 
 int main()
 {
+	string invoicenumber;
+	invoice myInvoice;
+
+	cout << "Invoice System v1.0\n";
+	getline(cin, invoicenumber);
+	cout << endl;
+
+	myInvoice.invoicedetails(invoicenumber);
 
 }
