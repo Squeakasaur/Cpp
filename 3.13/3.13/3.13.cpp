fac9df46d9782fb0db3f347ac6e6e20094a37a2d
Squeak <squeakasaur@live.com>
2014-07-09 20:10:39 -0700

    done?

--- 3.13/3.13/3.13.cpp
+++ 3.13/3.13/3.13.cpp
@@ -2,18 +2,23 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+/* invoice class*/
 class invoice
 {
 public:
 
-	void invoicedetails(string Details1,string Details2,string Details3, string Details4)
+	void invoicedetails(string Details1,string Details2,int Details3, int Details4, int Details5)
 	{
-		cout << "Invoice Number: " << Details1 << endl;
-		cout << "Customer: " << Details2 << endl;
-		cout << "Part Number: " << Details3 << endl;
+		cout << "Part Number: " << Details1 << endl;
+		cout << "Part Description: " << Details2 << endl;
+		cout << "Quantity: " << Details3 << endl;
 		cout << "Price: " << Details4 << endl;
+		cout << "Invoice Total: " << Details5 << endl;
+
+		
 	}
 
 
@@ -23,32 +28,41 @@ public:
 
 int main()
 {
-	string invoicenumber;
-	string customer;
+	
+	string descrip;
 	string part;
-	string price;
+	int price = 0;
+	int quantity = 0;
 	invoice myInvoice;
-
-	cout << "Invoice System v1.0" << endl;
-
-
-	cout << "Enter Invoice Number : ";
-	getline(cin, invoicenumber);
-	cout << endl;
-
 	
 
-	cout << "Enter Customer Name: ";
-	getline(cin, customer);
+	cout << "Invoice System v1.0\n\n" << endl; /* program title or welcome message*/
+
+
+	/* requests and stores the part number*/
+	cout << "Enter Part Number: "; 
+	getline(cin, part);   
 	cout << endl;
 	
-	cout << "Enter Part Number: ";
-	getline(cin, part);
+	/* requests and stores the part description */
+	cout << "Enter Part Description: ";
+	getline(cin, descrip);
+	cout << endl;
+	
+	/* requests and stores the part quantity */
+	cout << "Enter Quantity: ";
+	cin >> quantity;
 	cout << endl;
 
+	/* requests and stores the price */
 	cout << "Enter Price: ";
-	getline(cin, price);
+	cin >> price;
 	cout << endl;
+
+
+	/* declares variable total and multiplies quantity by price to give the invoice total*/
+	int total = quantity * price;
 	
-	myInvoice.invoicedetails(invoicenumber,customer, part, price);
+	myInvoice.invoicedetails(part,descrip, quantity, price, total);
+
 }
