263f2fd3add9105749e1b9194e2c38774d15d727
Squeak <squeakasaur@live.com>
2014-07-09 19:20:40 -0700

    Almost done

--- 3.13/3.13/3.13.cpp
+++ 3.13/3.13/3.13.cpp
@@ -8,11 +8,12 @@ class invoice
 {
 public:
 
-	void invoicedetails(string Details1, string Details2, string Details3, string Details4)
+	void invoicedetails(string Details1,string Details2,string Details3, string Details4)
 	{
-		cout << "Invoice Number\n" << Details1 << endl;
-		
-
+		cout << "Invoice Number: " << Details1 << endl;
+		cout << "Customer: " << Details2 << endl;
+		cout << "Part Number: " << Details3 << endl;
+		cout << "Price: " << Details4 << endl;
 	}
 
 
@@ -23,12 +24,31 @@ public:
 int main()
 {
 	string invoicenumber;
+	string customer;
+	string part;
+	string price;
 	invoice myInvoice;
 
-	cout << "Invoice System v1.0\n";
+	cout << "Invoice System v1.0" << endl;
+
+
+	cout << "Enter Invoice Number : ";
 	getline(cin, invoicenumber);
 	cout << endl;
 
-	myInvoice.invoicedetails(invoicenumber);
+	
 
+	cout << "Enter Customer Name: ";
+	getline(cin, customer);
+	cout << endl;
+	
+	cout << "Enter Part Number: ";
+	getline(cin, part);
+	cout << endl;
+
+	cout << "Enter Price: ";
+	getline(cin, price);
+	cout << endl;
+	
+	myInvoice.invoicedetails(invoicenumber,customer, part, price);
 }
